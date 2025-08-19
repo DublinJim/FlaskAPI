37836cf949d6bb9322cfc6d77ec9a3c736672948
DESKTOP-HS28MB3\dubli <dublinjim.jk@gmail.com>
2025-08-19 11:09:35 -0400

    now working basic test

---
+++ api.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Welcome to the API!</h1><p>This is a simple Flask API.</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
